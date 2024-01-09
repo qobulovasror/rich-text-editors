8d7a39ab337941ce1819bffc7363ffd923ae8690
qobulovasror <qobulovasror0@gmail.com>
2024-01-09 13:07:32 +0500

    add: react + draft.js

---
+++ react_and_draft/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
+import DOMPurify from 'dompurify';
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import './App.css';
+
+function App() {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  useEffect(() => {
+    let html = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(html);
+  }, [editorState]);
+
+  function createMarkup(html) {
+    return {
+      __html: DOMPurify.sanitize(html)
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Rich Text Editor Example
+      </header>
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={setEditorState}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+        toolbar={{
+          options: ['inline', 'blockType', "link", "history", "list"],
+        }}
+        hashtag={{
+          separator: ' ',
+          trigger: '#',
+        }}
+        mention={{
+          separator: ' ',
+          trigger: '@',
+          suggestions: [
+            { text: 'JavaScript', value: 'javascript', url: 'js' },
+            { text: 'Golang', value: 'golang', url: 'go' },
+          ],
+        }}
+      />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={createMarkup(convertedContent)}>
+      </div>
+    </div>
+  )
+}
+
+export default App;
